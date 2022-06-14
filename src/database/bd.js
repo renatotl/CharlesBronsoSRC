8ad0f176d6a9976b41f82acb8732493fc1245409
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-06-13 23:06:21 -0300

    s

--- src/database/bd.js
+++ src/database/bd.js
@@ -1,21 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DATABASE_URL,
-    {
-        
-        dialect: "postgres",
-        dialectOption: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
-    }
-);
-
-
-
+const sequelize = new Sequelize(process.env.DATABASE_URL, {   
+  dialect: "postgres", 
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,
+    },
+  },
+});
 
 
 module.exports = sequelize;
